0ba6b8edf27ee1a9b54b1380bb0ed16eca7bfb2a
SpatLyu <lyu.geosocial@gmail.com>
2024-08-30 14:41:59 +0800

    update

--- R/data.R
+++ R/data.R
@@ -3,7 +3,7 @@
 #' @description Spatial datasets of trace elements.
 #'
 #' @name obs
-#' @format \code{obs}: A tibble of trace elements with 614 rows and 5 variables
+#' @format \code{obs}: A data frame of trace elements with 614 rows and 5 variables
 #' @docType data
 #' @author Yongze Song \email{yongze.song@curtin.edu.au}
 #' @keywords dataset trace elements
@@ -38,7 +38,7 @@
 #' @description Spatial datasets of explanatory variables at a 500-m resolution.
 #'
 #' @name grids
-#' @format \code{grids}: A tibble of explanatory variables with 68757 rows and 11 columns.
+#' @format \code{grids}: A data frame of explanatory variables with 68757 rows and 11 columns.
 #' @docType data
 #' @author Yongze Song \email{yongze.song@curtin.edu.au}
 #' @keywords variables grids
