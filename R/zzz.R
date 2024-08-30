0442153e15d2c7edce626dee009377ad7e1cceb5
SpatLyu <lyu.geosocial@gmail.com>
2024-08-30 14:42:45 +0800

    remove

--- R/zzz.R
+++ R/zzz.R
@@ -4,7 +4,3 @@
                         \nSong, Y. (2022). The second dimension of spatial association. International Journal of Applied Earth Observation and Geoinformation. 2022. 111, 102834. doi: 10.1016/j.jag.2022.102834.
                         ")
 }
-
-.onLoad = function(...) {
-  loadNamespace("tibble")
-}
