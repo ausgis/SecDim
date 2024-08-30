9b26e66367c8053bf3cbed104c5e63e8c19e046b
SpatLyu <lyu.geosocial@gmail.com>
2024-08-30 13:49:56 +0800

    add zzz

---
+++ R/zzz.R
@@ -0,0 +1,10 @@
+.onAttach = function(...){
+  packageStartupMessage("This is `SecDim` 3.3.
+                        \nTo cite `SecDim` in publications, please use:
+                        \nSong, Y. (2022). The second dimension of spatial association. International Journal of Applied Earth Observation and Geoinformation. 2022. 111, 102834. doi: 10.1016/j.jag.2022.102834.
+                        ")
+}
+
+.onLoad = function(...) {
+  loadNamespace("tibble")
+}
